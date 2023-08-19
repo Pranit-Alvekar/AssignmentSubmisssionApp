8539c2c7ed407e77190c76cb253cd5b4e081044d
xvivex <vivek.dalvi333@gmail.com>
2023-08-20 01:24:42 +0530

    added crud for assignment entity

--- back-end/AssignmentSubmissionApp/src/main/java/com/codereview/AssignmentSubmissionApp/repository/AssignmentRepository.java
+++ back-end/AssignmentSubmissionApp/src/main/java/com/codereview/AssignmentSubmissionApp/repository/AssignmentRepository.java
@@ -3,7 +3,11 @@ package com.codereview.AssignmentSubmissionApp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codereview.AssignmentSubmissionApp.domain.Assignment;
+import com.codereview.AssignmentSubmissionApp.domain.User;
+
+import java.util.Set;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long>{
-
+	
+		Set<Assignment> findByUser(User user);
 }
--- back-end/AssignmentSubmissionApp/src/main/java/com/codereview/AssignmentSubmissionApp/service/AssignmentService.java
+++ back-end/AssignmentSubmissionApp/src/main/java/com/codereview/AssignmentSubmissionApp/service/AssignmentService.java
@@ -1,5 +1,8 @@
 package com.codereview.AssignmentSubmissionApp.service;
 
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,20 @@ public class AssignmentService {
 		assignment.setUser(user);
 		
 		return assignmentRepo.save(assignment);
+	}
+	
+	public Set<Assignment> findByUser(User user){
+		return assignmentRepo.findByUser(user);
+	}
+
+	public Optional<Assignment> findById(Long assignmentId) {
+		return assignmentRepo.findById(assignmentId);
+		
+	}
+
+	public Assignment save(Assignment assignment) {
+		return assignmentRepo.save(assignment);
+		
 	}
 
 }
