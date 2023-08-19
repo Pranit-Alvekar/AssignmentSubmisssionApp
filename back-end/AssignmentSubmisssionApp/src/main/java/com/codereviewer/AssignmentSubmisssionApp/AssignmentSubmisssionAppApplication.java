170c490cae6f1e7679b919afde96c91fb17860a6
xvivex <vivek.dalvi333@gmail.com>
2023-08-19 15:41:33 +0530

    do

---
+++ back-end/AssignmentSubmisssionApp/src/main/java/com/codereviewer/AssignmentSubmisssionApp/AssignmentSubmisssionAppApplication.java
@@ -0,0 +1,13 @@
+package com.codereviewer.AssignmentSubmisssionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmisssionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSubmisssionAppApplication.class, args);
+	}
+
+}
---
+++ back-end/AssignmentSubmisssionApp/src/test/java/com/codereviewer/AssignmentSubmisssionApp/AssignmentSubmisssionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codereviewer.AssignmentSubmisssionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmisssionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
