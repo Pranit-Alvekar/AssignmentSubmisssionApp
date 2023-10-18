9781363fbe443e191b6413ef1fe9d3b76e3b511f
xvivex <124356676+xvivex@users.noreply.github.com>
2023-10-19 00:39:32 +0530

    dockerized app as whole

--- back-end/src/main/java/com/codereview/AssignmentSubmissionApp/enums/AuthorityEnum.java
+++
@@ -1,9 +0,0 @@
-package com.codereview.AssignmentSubmissionApp.enums;
-
-public enum AuthorityEnum {
-	
-	ROLE_STUDENT,
-	ROLE_CODE_REVIEWER,
-	ROLE_ADMIN
-
-}
--- back-end/src/main/java/com/codereview/AssignmentSubmissionApp/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codereview.AssignmentSubmissionApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codereview.AssignmentSubmissionApp.domain.Authority;
-
-public interface AuthorityRepository  extends JpaRepository<Authority,Long>{
-
-}
